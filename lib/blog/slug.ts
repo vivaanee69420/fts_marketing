a8cf65e38fc31fa2a4692aaf167fc07e4bc5a77e
ruhithpasha <ruhithpasha813@gmail.com>
2026-06-24 00:01:29 +0530

    feat: slugify utility

---
+++ lib/blog/slug.ts
@@ -0,0 +1,8 @@
+export function slugify(input: string): string {
+  return input
+    .normalize("NFKD")
+    .replace(/[̀-ͯ]/g, "") // strip diacritics
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
